91af588e1b1d1e121e284e44e7ab5f7025aef44d
Nikkune <39634259+Nikkune@users.noreply.github.com>
2023-09-04 11:48:13 +0200

    Remake backend

--- backend/app.js
+++ backend/app.js
@@ -1,56 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config');
-const getLogger = require('./utils/Logger');
-const keyGen = require('./utils/KeyGen');
-const StoryNodesRouter = require('./routes/StoryNodesRouter').router;
-const StoryEdgesRouter = require('./routes/StoryEdgesRouter').router;
-const UsersRouter = require('./routes/UsersRouter').router;
-
-const corsOptions = {
-    origin: config.allowedOrigins, // Utilisez l'option allowedOrigins pour spécifier les domaines autorisés
-};
 
 const app = express();
 
-app.use(cors(corsOptions));
+// Middleware
+// Middleware pour autoriser les requêtes depuis les domaines autorisés
+app.use(cors({
+    origin: config.allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
 
+// Middleware pour analyser le corps des requêtes au format JSON
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    const apiKey = req.headers['x-api-key'];
+// Middleware pour analyser les cookies
+app.use(cookieParser());
 
-    if (!apiKey) {
-        getLogger('App').error('Tentative d\'accès sans clé API.');
-    } else {
-        if (!config.allowedApiKeys.includes(apiKey)) {
-            getLogger('App').error('Tentative d\'accès non autorisée avec l\'API : ' + apiKey);
-            return res.status(401).json({error: 'Accès non autorisé.'});
-        }
-    }
-
-    const logger = getLogger(apiKey);
-    if (!config.allowedRoutes[apiKey].includes(req.url.split('/')[1])) {
-        logger.error(`Requête ${req.method.charAt(0).toUpperCase()}${req.method.substr(1).toLowerCase()} ${req.url} non autorisé`);
-        getLogger('App').error(`Requête ${req.method.charAt(0).toUpperCase()}${req.method.substr(1).toLowerCase()} ${req.url} non autorisé pour : ${apiKey}`);
-        return res.status(401).json({error: 'Accès non autorisé.'});
-    }
-
-    logger.info(`Requête ${req.method.charAt(0).toUpperCase()}${req.method.substr(1).toLowerCase()} ${req.url} autorisée`);
-
-    next();
-});
-
-app.use('/storyNodes/', StoryNodesRouter);
-app.use('/storyEdges/', StoryEdgesRouter);
-app.use('/users/', UsersRouter);
-
-app.get('/token', (req, res) => {
-    res.send(keyGen(req.body.length));
-})
 
 app.listen(config.port, () => {
-    getLogger('App').info(`Serveur Express écoutant sur le port ${config.port}`);
-    console.log(`Serveur Express écoutant sur le port ${config.port}`);
+    config.logger.info(`Serveur en écoute sur le port ${config.port}`);
 });
